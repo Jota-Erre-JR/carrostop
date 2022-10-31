7710e465cb35cbc46648ee84137161b40de16b62
Julio Correa <juliocesarjr@outlook.com>
2022-10-31 11:33:53 -0300

    Bootstrap theme customization

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import './assets/styles/custom.scss';
 
 import './App.css';
 
 function App() {
   return (
-    <h1>Carros Top</h1>
+    <h1 className="bg-primary">Carros Top</h1>
   );
 }
 
